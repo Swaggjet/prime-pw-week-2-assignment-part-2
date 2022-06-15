f4f38eaf0a4200abed21474b49bebc42c16e2f6b
Swaggjet <asantesimmons@gmail.com>
2022-06-15 11:43:41 -0500

    week2pt2

---
+++ describe.js
@@ -0,0 +1,175 @@
+// WHAT IS THIS EVEN DOING?
+
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// They are in isolation -- variables in one question do not affect other questions.
+// Describe the code as it would be run, and make sure you give us the console.log's value at the end.
+// (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
+
+
+// 0.
+// //EXAMPLE. We make a variable called number and set it to 1 as a number.
+// Then we increment the number variable. Number is now 2.
+// We check if number is greater than or equal to 2. 2 is equal to 2, so
+// we console.log 'yes'.
+
+//CODE
+/*
+let number = 1;
+number++;
+if (number >= 2) {
+  console.log('yes');
+}
+*/
+
+
+// 1. WRITE YOUR DESCRIPTION HERE
+// "Let" is assigning the varible "name" to equal 'Dane'
+// The "if" command is saying if 'name' is equal to Mary show a log
+// of 'Hi Mary' also show log of 'How do you do?'
+//
+
+//CODE
+/*
+let name = 'Dane';
+if (name === 'Mary') {
+  console.log('Hi, Mary!');
+} else {
+  console.log('How do you do?');
+}
+*/
+
+// 2. WRITE YOUR DESCRIPTION HERE
+// let there be varible of 'secret'. Let there be a varible of 'code' equal to 123
+// If 'code' is equal to 123 then 'secret' will become equal to 'Super'
+// 'code' also become equal to 'code' times 2.
+// If 'code' greater than 250 then secret equal to 'duper'
+// also log 'Secret'
+
+//CODE
+/*
+let secret;
+let code = 123;
+if(code === 123) {
+  secret = 'super';
+  code = code * 2;
+}
+if (code > 250) {
+  secret = 'duper'
+}
+console.log(secret)
+*/
+
+// 3. WRITE YOUR DESCRIPTION HERE
+// create 'isStudent' to be equal to 'true'. create 'age' be equal to 34. create 'zip' to be equal to 55407
+// the If statement is saying if 'isStudent' equal to 'true' and 'zip' greater than 80000
+// log 'you're a student on the west coast'. also if  'isStudent' equal 'false' or 'age' less than 30
+// log 'what are your hobbies?'
+// create 'isStudent' equal to 'true' then log 'welcome to prime'
+// also log 'How about the weather?'
+
+//CODE
+/*
+let isStudent = true;
+let age = 34;
+let zip = 55407;
+if (isStudent === true && zip > 80000 ) {
+  console.log(`You're a student on the West Coast!`);
+} else if (isStudent === false || age < 30) {
+  console.log('What are your hobbies?');
+} else if (isStudent === true) {
+  console.log('Welcome to Prime!');
+} else {
+  console.log('How about the weather?')
+}
+*/
+
+
+
+
+// REVERSE
+// Now, for questions 4-6, we'll give you a description and some code.
+// Some of the code will NOT match the description.
+// Comment above the problem code, and describe a potential solution.
+// Look closely -- things are logically incorrect or missing and wrong in the code!
+
+// EXAMPLE
+// 0. (DESCRIPTION OF CORRECT BEHAVIOR)
+// We make a variable called number and set it to 1 as a number.
+// Then we increment the number variable. Number is now 2.
+// We check if number is greater than or equal to 2.
+// -- it is, so we console.log 'yes'
+
+//CODE
+/*
+let number = 1;
+// FIX - number-- decrements number, but the instructions ask to increment.
+// Should be number++.
+number--;
+if (number >= 2) {
+  console.log('yes');
+}
+*/
+
+
+
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
+// -- it is, so we set colorOne and colorTwo to 'purple'
+
+/*
+let colorOne = 'red';
+let colorTwo = 'blue';
+let mix = true;
+if (mix === true) {
+  colorOne = 'purple';
+}
+*/
+
+// I would add in the if statement colorTwo and add an "+" to make sure both color are equal to 'purple'
+//if (mix === true) {
+//  colorOne + colorTwo = 'purple';
+// }
+
+//5. We start with two variables -- temp is set to 40 and time is set to 4.
+// We check if temp is higher than 39 and if time is greater or equal to  4
+// -- they are so we console.log 'throw away the food!'
+
+/*
+let temp = 40;
+const time = 4;
+if (temp > 39 || time >= 4) {
+  console.log('throw away the food!');
+}
+*/
+
+// The only change to the code would be '||' should be '&&' to make the "if" statetment accurate.
+
+/*
+let temp = 40;
+const time = 4;
+if (temp > 39 && time >= 4) {
+  console.log('throw away the food!');
+}
+*/
+
+//6. We start with two variables -- age is set to 21 and minAge is set to 21.
+// We check if age is greater than or equal to minAge
+// -- it is, so we console.log 'enter'.
+
+/*
+let age = 21;
+const minAge = 21;
+if(minAge <= age) {
+  console.log('no entry');
+} else {
+  console.log('enter');
+}
+*/
+// The only needed to be change would be console log need to be changed
+//
+//  if(minAge <= age) {
+//  console.log('enter');
+// } else {
+//  console.log('no entry');
+// }
+
